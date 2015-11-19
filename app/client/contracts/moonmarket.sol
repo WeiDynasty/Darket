52a93a8e7424ce1c08d89e0c91974a2bf17e7a41
nginnever <ginneversource@gmail.com>
2015-11-18 20:57:03 -0800

    more advanced

--- app/client/contracts/moonmarket.sol
+++ app/client/contracts/moonmarket.sol
@@ -2,18 +2,25 @@ contract MoonMarket {
     
     address public owner;
     mapping (bytes32 => address) public markets;
+    mapping (address => address[])[] data;
 
     
     function MoonMarket() {
     	owner = msg.sender;
     }
 
-    function createMarket() returns (address mAddr){
-       return address(new Market());
+    function addMarket(bytes32 name, address addr) returns (bool result) {
+        Market mar = Market(addr);
+
+        if(!mar.setMarketAddress(address(this))) {
+            return false;
+        }
+        markets[name] = addr;
+        return true;
     }
 
     function deleteMarket(address dmAddr){
-        Market(dmAddr).remove();
+        //todo delete function 
     }
     
     function remove() {
@@ -31,6 +38,14 @@ contract Market {
 
     function Market() {
     	admin = msg.sender;
+    }
+
+    function setMarketAddress(address marketAddr) returns (bool result){
+        if(admin != 0x0 && marketAddr != admin){
+            return false;
+        }
+        admin = marketAddr;
+        return true;
     }
 
     function remove() {
